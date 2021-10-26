8959725da46b6e6bafee0781ab3ab2e8641455c6
lgpgomes <gutto-pg@hotmail.com>
2021-10-25 21:41:59 -0300

    corrigido bug do login/cadastro

---
+++ README.md
@@ -0,0 +1,7 @@
+### Checklist
+
+- [x] Fazer a tela principal;
+- [x] Fazer a tela de login/cadastro;
+- [ ] Fazer validações do login/cadastro;
+- [ ] Fazer a tela de agendamento;
+- [ ] Fazer o paranauê do DB;
---
+++ assets/js/main.js
@@ -0,0 +1,69 @@
+
+(function ($) {
+    "use strict";
+
+    
+    /*==================================================================
+    [ Validate ]*/
+    var name = $('.validate-input input[name="name"]');
+    var email = $('.validate-input input[name="email"]');
+    var message = $('.validate-input textarea[name="message"]');
+
+
+    $('.validate-form').on('submit',function(){
+        var check = true;
+
+        if($(name).val().trim() == ''){
+            showValidate(name);
+            check=false;
+        }
+
+
+        if($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+            showValidate(email);
+            check=false;
+        }
+
+        if($(message).val().trim() == ''){
+            showValidate(message);
+            check=false;
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .input2').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+       });
+    });
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+    //Função fadeOut no preloader
+    $(window).on('load', function () {
+        $('#preloader .inner').fadeOut();
+        $('#preloader').delay(350).fadeOut('slow'); 
+        $('body').delay(350).css({'overflow': 'visible'});
+      });
+      //Adiciona classe nav-bg
+      var nav = document.querySelector('nav');
+      window.addEventListener('scroll', function () {
+        if (window.pageYOffset > 0) {
+          nav.classList.add('nav-bg');
+        } else {
+          nav.classList.remove('nav-bg');
+        }
+      });
+
+})(jQuery);
